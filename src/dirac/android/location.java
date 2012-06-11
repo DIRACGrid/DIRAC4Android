bcd05f24078c298aaebea58757bc9dbf6858d754
Cedric Potterat <potterat@ecm.ub.es>
2012-06-11 11:38:01 +0200

    test json with TwitterTrend

---
+++ src/dirac/android/location.java
@@ -0,0 +1,20 @@
+package dirac.android;
+
+public class location {
+
+    private String name;
+    private int woeid;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setWoeid(int woeid) {
+        this.woeid = woeid;
+    }
+    public int getWoeid() {
+        return woeid;
+    }
+}
