07d7743a816f6ac6bbab32f0588c116e670eb9a3
Cedric Potterat <potterat@ecm.ub.es>
2012-06-21 17:48:59 +0200

    splash screen and icon

---
+++ src/dirac/android/SplashScreen.java
@@ -0,0 +1,35 @@
+package dirac.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+public class SplashScreen extends Activity {
+    private static final int STOPSPLASH = 0;
+    private static final long SPLASHTIME = 5000;
+ 
+    private Handler splashHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case STOPSPLASH:
+                    //remove SplashScreen from view
+                    Intent intent = new Intent(SplashScreen.this, DIRACAndroidActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        Message msg = new Message();
+        msg.what = STOPSPLASH;
+        splashHandler.sendMessageDelayed(msg, SPLASHTIME);
+    }
+}
